44e918c5b8ea2cc39f2808d21485c2049ccd34b5
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-09-21 13:58:25 +0200

    TEST

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import { createStore } from 'vuex'
-import products from './products'
-import auth from './auth'
-import cart from './cart'
+import { createStore } from "vuex";
+import products from "./products";
+import auth from "./auth";
+import cart from "./cart";
+
+console.log("KOOOOOLEGA");
 
 export default createStore({
   modules: {
     products,
     auth,
-    cart
-  }
-})
+    cart,
+  },
+});
